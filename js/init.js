fad798ae0fa90f8b5e7005f77f49cdc726a5686c
gowebautentic <gowebautentic@gmail.com>
2021-11-17 14:04:17 -0600

    ok

--- js/init.js
+++ js/init.js
@@ -5,12 +5,11 @@ firebase.initializeApp({
 <<<<<<< HEAD
   apiKey: "AIzaSyCrtzot0ah7OFa5gIC-viTdBNZNPNkpjPQ",
 =======
-   apiKey: "AIzaSyCrtzot0ah7OFa5gIC-viTdBNZNPNkpjPQ",
->>>>>>> a2e5841d8543fdf6c2770a2b0b84bb10c03de4d0
-  authDomain: "gowebautenticch.firebaseapp.com",
-  projectId: "gowebautenticch",
-  storageBucket: "gowebautenticch.appspot.com",
-  messagingSenderId: "801769334258",
-  appId: "1:801769334258:web:31984d9437c6870f1f376b",
-  measurementId: "G-69BH5BQQNT"
+apiKey: "AIzaSyCrtzot0ah7OFa5gIC-viTdBNZNPNkpjPQ",
+authDomain: "gowebautenticch.firebaseapp.com",
+projectId: "gowebautenticch",
+storageBucket: "gowebautenticch.appspot.com",
+messagingSenderId: "801769334258",
+appId: "1:801769334258:web:31984d9437c6870f1f376b",
+measurementId: "G-69BH5BQQNT"
     });
